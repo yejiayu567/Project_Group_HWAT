148b389c81b7e926a9116ad11a065406470b3be9
yejiayu567 <123612481+yejiayu567@users.noreply.github.com>
2023-01-26 17:48:25 +0800

    stage 1

---
+++ Main.py
@@ -0,0 +1,15 @@
+# import cash_on_hand, overheads, profit_loss
+# from utils.read_data import read_data
+
+# def main():
+
+#     cash_on_hand_data = read_data('csv_reports/cash-on-hand.csv')
+#     overheads_data = read_data('csv_reports/Overheads.csv')
+#     profit_loss_data = read_data('csv_reports/profit-and-loss.csv')
+    
+#     overheads.overhead_function(overheads_data)
+#     cash_on_hand.coh_function(cash_on_hand_data)
+#     profit_loss.profitloss_function(profit_loss_data)
+
+    
+# main()
