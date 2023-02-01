9b1891ff08bfb010afea87c847a3f00c74c5658c
yejiayu567 <123612481+yejiayu567@users.noreply.github.com>
2023-02-01 13:07:49 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 # import modules
 import cash_on_hand, overheads, profit_loss
 
-
-# call function from adder module
 def main():
+    """
+    This function will call functions from adder modules
+    No parameter is required
+    """
+    
     filename = 'summary_report.txt'
     overheads.Overhead_Function(filename)
     cash_on_hand.COH_Function(filename)
