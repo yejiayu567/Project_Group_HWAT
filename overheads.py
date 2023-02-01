95ec81d0de0b021cbf68adad3b81641f0fb0ef58
chloesytan <123613257+chloesytan@users.noreply.github.com>
2023-02-01 16:40:41 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -19,13 +19,18 @@ def Overhead_Function(filename):
         # iterate each row with loop
         for row in reader:
             data.append([(row[0]), (float(row[1]))])
-    # find the highest overheads and category
+    # setting max_category to keep track of the day when the highest value of the net profit occurred
     max_category = data[0][0]
+    #  setting max_overheads to keep track of the highest value of the net profit
     max_overheads = data[0][1]
     # iterate each data with loop
     for index in range(1, len(data)):
         if data[index][1] > max_overheads:
+            # If a value is found that is higher than the current maximum,
+            # the value of max_category can be updated to reflect the new maximum.
             max_category = data[index][0]
+            # If a value is found that is higher than the current maximum, 
+            # the value of max_overheads can be updated to reflect the new maximum.
             max_overheads = data[index][1]
             
     # write the statement to the txt file
