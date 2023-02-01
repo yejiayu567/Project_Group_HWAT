522e7265ec95f5232c7c5c93ccc9b357f387d2ec
chloesytan <123613257+chloesytan@users.noreply.github.com>
2023-02-01 16:29:21 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -18,20 +18,26 @@ def Profit_Loss_Function(filename):
         # create empty lists to store value
         data = []
         # iterate each row with loop
+        # 
         for row in reader:
             data.append([(row[0]), (float(row[4]))])
     # sort the data by date
     sorted(data)
     # determine whether the data is surplus
     surplus = True
+    #setting a for loop that will run through the data in data list by finding the number of elements by len(data)
     for index in range(len(data) - 1):
-        # find the deficit value by calculating the difference
+        # check if the net profit of the next day (data[index + 1][1]) is less than the net profit of the current day (data[index][1]).
         if data[index + 1][1] < data[index][1]:
+            # if net profit of the next day is less than the net profit of the current day, surplus will return false
             surplus = False
+            # finding the day value for the next row of data
             day = data[index + 1][0]
+            # calculating the difference between the net profit of two consecutive days, and assigning it to the variable dif.
             dif = data[index][1] - data[index + 1][1]
             # write the statement into the txt file
             with open(filename, mode='a') as f:
+                # append to the end of the txt file
                 f.write(f'[PROFIT DEFICIT]DAY: ({day}:.1f), AMOUNT: USD{dif}')
 
     # if the data is surplus, return the statement to the txt file
